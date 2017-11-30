b8e981b0fb8ada3fa758da1f3ea44188c370bb75
Sgt DE JESUS <1109888279@rede.eme>
2017-11-30 13:56:43 -0200

    Inclusao da pagina de listagem de Usuarios

---
+++ src/main/java/com/aewinformatica/brewer/repository/Grupos.java
@@ -0,0 +1,9 @@
+package com.aewinformatica.brewer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aewinformatica.brewer.model.Grupo;
+
+public interface Grupos extends JpaRepository<Grupo, Long> {
+
+}
